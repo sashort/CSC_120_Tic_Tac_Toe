02c3f678537697fd420c482687e25c6616a89dde
Stephen Short <essayshort@gmail.com>
2021-12-09 23:52:20 -0500

    Final Implementation

--- board.py/board.py
+++ board.py/board.py
@@ -1,3 +1,6 @@
+global board
+
+
 def new_board():
     return [
         ['-', '-', '-'],
@@ -6,64 +9,93 @@ def new_board():
     ]
 
 
-def print_board(board):
-    print("╭───────╮")
+def print_board(message='', new_line=True):
+    pad_screen()
+    output = "╭───────╮\n"
     for row in board:
-        print("│ ", end='')
+        output += "│ "
         for val in row:
-            print(val + ' ', end='')
-        print('│')
-    print("╰───────╯")
+            output += val + ' '
+        output += '│\n'
+    output += "╰───────╯"
+    output += str('\n' + message) if message != '' else ''
+    print(output, end='\n' if new_line else '')
 
 
 def pad_screen():
-    i = 0;
+    i = 0
     while i < 100:
-        print();
+        print()
         i += 1
 
 
 def main():
+    global board
     board = new_board()
-    turn(board)
+    print_board(turn_loop())
 
 
-def turn(board):
+def turn_loop():
     # TODO True will be replaced with a function that checks for a winner, which will break the loop.
     is_player1 = True
+    message = ''
     while True:
-        pad_screen()
-        print_board(board)
-        while True:
-            try:
-                # get input and convert it into a row/column numbers
-                print("[", "X" if is_player1 else "O", "] Select your next move")
-                loc = input("ROW COLUMN | ")
-                vals = loc.replace(',', '').split(' ')
-                row = int(vals[0])
-                column = int(vals[1])
+        print_board(message +
+                    ('\n' if message != '' else '') +
+                    "[ " + ("X" if is_player1 else "O") +
+                    " ] Select your next move")
 
-                if not 0 < row * column < 10:
-                    raise ValueError
+        message = mark_board(input("ROW COLUMN | "), 'X' if is_player1 else 'O')
 
-                if board[row-1][column-1] != '-':
-                    print("----------------------------------------------------")
-                    print("  That spot has already been used!")
-                    print("  Please select a blank space!!")
-                    print("----------------------------------------------------")
-                    continue
-                else:
-                    board[row-1][column-1] = 'X' if is_player1 else 'O'
-                    is_player1 = not is_player1
-                    break
+        if message == 'X Wins' or message == 'O Wins':
+            return message
+        elif message == '':
+            is_player1 = not is_player1
 
-            except ValueError:
-                print("====================================================")
-                print("  I'm sorry, I didn't recognize your input!")
-                print("  Please specify a number 1-3 for the ROW and COLUMN")
-                print("====================================================")
+
+def mark_board(coords_string, player):
+    try:
+        vals = coords_string.replace(',', '').split(' ')
+        row = int(vals[0])
+        col = int(vals[1])
+
+        if not 0 < row * col < 10:
+            raise ValueError
+
+        if board[row - 1][col - 1] != '-':
+            return ("----------------------------------------------------\n" +
+                    "  I'm sorry, that space is already taken!\n" +
+                    "  Please select an empty space to place your mark.\n" +
+                    "----------------------------------------------------\n")
+
+        board[row - 1][col - 1] = player
+        return check_winner()
+    except ValueError:
+        return ("====================================================\n" +
+                "  I'm sorry, I didn't recognize your input!\n" +
+                "  Please specify a number 1-3 for the ROW and COLUMN\n" +
+                "====================================================\n")
+
+
+def check_winner():
+    win_string = ('|' + board[0][0] + board[0][1] + board[0][2] + '|' +
+                  '|' + board[1][0] + board[1][1] + board[1][2] + '|' +
+                  '|' + board[2][0] + board[2][1] + board[2][2] + '|' +
+                  '|' + board[0][0] + board[1][0] + board[2][0] + '|' +
+                  '|' + board[0][1] + board[1][1] + board[2][1] + '|' +
+                  '|' + board[0][2] + board[1][2] + board[2][2] + '|' +
+                  '|' + board[0][0] + board[1][1] + board[2][2] + '|' +
+                  '|' + board[2][0] + board[1][1] + board[0][2] + '|')
+
+    try:
+        if win_string.index('|XXX|') > -1:
+            return "X Wins"
+    except ValueError:
+        try:
+            if win_string.index("|OOO|") > -1:
+                return "O Wins"
+        except ValueError:
+            return ''
 
 
 main()
-
-
